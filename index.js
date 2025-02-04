4ca346e3b57f02a6fe8a860f55391a728d875407
Ubuntu <anilTyata@dlytica-devops-anil.cluster.local>
2025-02-04 06:09:38 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World! This is a Node.js app running in Docker.');
+  res.send('Hello, World! This is a Node.js app running in Docker1.');
 });
 
 app.listen(PORT, () => {
