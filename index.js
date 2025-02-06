6a3632319c6b25b43b20edfcf4e4615cc1938205
Ubuntu <anilTyata@dlytica-devops-anil.cluster.local>
2025-02-06 11:14:21 +0000

    Test CI/CD update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World! This is a Node.js app running in Docker1.');
+  res.send('Hello, World! This is a Node.js app running in Docker test 2.');
 });
 
 app.listen(PORT, () => {
